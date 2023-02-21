f62d01191d7d90939b42978905aec2e8ae5f4135
Trent Osterman <trentosterman@gmail.com>
2023-02-21 13:43:12 -0600

    not quite finished but made my best attempt.

--- client.js
+++ client.js
@@ -32,7 +32,7 @@ const employees = [
   }
 ];
 
-console.log('array of employee data: ',  employees );
+console.log('array of employee data: ', employees);
 
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -48,10 +48,65 @@ console.log('array of employee data: ',  employees );
 
 // This function will calculate 1 employee's bonus!
 //
-function calculateIndividualEmployeeBonus( employee ) {  
+function calculateIndividualEmployeeBonus(employee) {
+  let totalBonus = 0;
+  let bonusPercentage = 0;
+  let totalCompensation = 0;
   // your logic here
-  
-  
-  // return new object with bonus results
+  console.log(employee.name)
+  console.log(employee.annualSalary)
 
+  //Those who have a rating of a 2 or below should not receive a bonus.
+  if (employee.reviewRating < 2) {
+    console.log('review rating is less than 2:', employee.reviewRating)
+    bonusPercentage = 0;
+    console.log('current bonus percentage:', bonusPercentage);
+  }
+
+  // Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+  if (employee.reviewRating === 3) {
+    console.log('review rating is equal than 3:', employee.reviewRating)
+    bonusPercentage = .04;
+    console.log('current bonus percentage:', bonusPercentage);
+  }
+
+  // Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+  if (employee.reviewRating === 4) {
+    console.log('review rating is equal than 4:', employee.reviewRating)
+    bonusPercentage = .06;
+    console.log('current bonus percentage:', bonusPercentage);
+  }
+
+  // Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+  if (employee.reviewRating === 5) {
+    console.log('review rating is equal than 5:', employee.reviewRating)
+    bonusPercentage = .1;
+    console.log('current bonus percentage:', bonusPercentage);
+  } 
+
+  /* If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
+   and should receive an additional 5%
+  */
+  if (employee.employeeNumber.length > 4) {
+    bonusPercentage = bonusPercentage + .05;
+  } 
+
+  // However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+  // if (employee.annualSalary < 65000) {
+  //   bonusPercentage = bonusPercentage - .01;
+  // }
+
+// creating a new object for each employee with the bonus percentage, bonus, and total compensation.
+  return {
+    name: employee.name,
+    bonusPercentage: bonusPercentage, // test value
+    totalCompensation: totalCompensation, // test value
+    totalBonus: totalBonus, //test value
+  }
+}
+
+
+// crating a loop to run the function over every employee.
+for (let employee of employees) {
+  calculateIndividualEmployeeBonus(employee)
 }
